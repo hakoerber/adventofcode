45bb61e4fdd7134a96c8a9d0e5661b69c848604f
Hannes Körber <hannes@hkoerber.de>
2023-12-07 20:10:26 +0100

    Add script to run all parts

---
+++ 2023/all.sh
@@ -0,0 +1,36 @@
+#/usr/bin/env bash
+
+set -o nounset
+set -o errexit
+
+for day in ./day* ; do
+  (cd "${day}" && cargo build --release)
+done
+
+run() {
+  echo "day1"
+  ./day1/target/release/day1
+
+  echo "day2"
+  ./day2/target/release/day2
+
+  echo "day3"
+  ./day3/target/release/day3
+
+  echo "day4"
+  ./day4/target/release/day4
+
+  echo "day5"
+  ./day5/target/release/day5 1 
+  ./day5/target/release/day5 2 ranges
+
+  echo "day6"
+  ./day6/target/release/day6 1 quadraticformula
+  ./day6/target/release/day6 2 quadraticformula
+
+  echo "day7"
+  ./day7/target/release/day7 1
+  ./day7/target/release/day7 2
+}
+
+time run
